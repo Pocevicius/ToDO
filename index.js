1daad9ce1285dd2c8885ee6a3ff3c17f2be2fec4
Pocevicius <lukas.pocevicius@gmail.com>
2022-08-02 21:34:55 +0300

    delete buton works with tasks

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
 let arr = [
   {
-    id: 123,
+    id: 111,
     task: "buy milk",
   },
   {
-    id: 321,
+    id: 222,
     task: "buy egg",
   },
   {
-    id: 222,
+    id: 333,
     task: "watch any js video",
   },
   {
-    id: 333,
+    id: 444,
     task: "learn about js",
   },
 ];
 document.getElementById("batonas").addEventListener("click", () => {
   const inputValue = document.getElementById("taskInput").value;
-  const newTask = { id: new Date().getTime(), task: inputValue };
   const form = document.getElementById("taskInput");
-  form.value = "";
-  if (form.value.length == 0) {
-    alert("please enter task");
-    return false;
+  //tikrinam ar tuscias input stringas
+  if (!inputValue.trim()) {
+    alert("NO TASK ENTERED");
+    //trina para6yta forma//
+    form.value = "";
+    return;
   }
+  const newTask = { id: new Date().getTime(), task: inputValue };
+  form.value = "";
+
   arr.unshift(newTask);
   drawTaskList();
 });
 
+function deleteItem() {
+  console.log(this.id);
+  arr = arr.filter((val) => val.id !== +this.id);
+
+  drawTaskList();
+}
+
 const drawTaskList = () => {
   const tasksList = document.getElementById("tasksList");
   tasksList.innerHTML = null;
-  arr.forEach((value) => {
+  arr.forEach((value, ind) => {
     //creating elements//
     const myLI = document.createElement("li");
     const myInput = document.createElement("input");
     const myLabel = document.createElement("label");
+    const deleteBtn = document.createElement("button");
     //adding styles//
-    myLI.className = "list-group-item";
-    myInput.className = "form-check-input me-1";
-    myLabel.className = "form-check-label";
+    myLI.className = "container list-group-item ";
+    myInput.className = "form-check-input me-1 col-2";
+    myLabel.className = "form-check-label col-8";
+    deleteBtn.className = "btn btn-warning btn-sm col-2";
 
     //adding other attributes
     //input
     myInput.setAttribute("type", "checkbox");
-    myInput.setAttribute("id", value.id);
+    myInput.setAttribute("id", ind);
     //label
-    myLabel.setAttribute("for", value.id);
+    myLabel.setAttribute("for", ind);
     myLabel.textContent = value.task;
+    //delete btn
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.setAttribute("id", value.id);
+    deleteBtn.textContent = "delete";
+    deleteBtn.onclick = deleteItem;
     //apend childs
-    myLI.append(myInput, myLabel);
+    myLI.append(myInput, myLabel, deleteBtn);
     tasksList.append(myLI);
   });
 };
